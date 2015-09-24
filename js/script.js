99437f506a5fb8dd70f16e6d269ea0bd30f455bf
sheez <blaat2k@mail.com>
2015-09-24 22:39:56 +0200

    Add working resize bug fix

--- js/script.js
+++ js/script.js
@@ -64,14 +64,14 @@ $(document).ready(function() {
             $header.removeClass("menuHeight");
             $nav.css("display", "none");
         }
+    });
 
-        $(window).resize(function() {
-           if(!$header.hasClass('menuHeight') && $(window).height() > 768) {
+    $(window).resize(function() {
+           if(!$header.hasClass('menuHeight') && $(window).width() > 768) {
                $nav.css("display", "block");
                $header.addClass("menuHeight");
            }
         });
-    });
 
     $.getJSON('dates.json', function(data) {
         drawTable(data);
