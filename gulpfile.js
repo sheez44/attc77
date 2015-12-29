973a0e6717337a38414f7b7829b2782e9711967c
sheez <blaat2k@mail.com>
2015-12-29 15:24:06 +0100

    Add compass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
 var gulp = require('gulp'),
-    concat = require('gulp-concat');
-    browserify = require('gulp-browserify');
+    concat = require('gulp-concat'),
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass');
 
 var jsSources = [
     'components/js/*.js'
 ];
+
+var sassSources = ['components/scss/application.scss'];
 
 gulp.task('js', function() {
     gulp.src(jsSources)
         .pipe(concat('scripts.js'))
         .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function() {
+   gulp.src(sassSources)
+       .pipe(compass({
+           sass: 'components/scss',
+           image: 'components/img',
+           style: 'expanded'
+       }))
+       .pipe(gulp.dest('builds/development/css'))
 });
